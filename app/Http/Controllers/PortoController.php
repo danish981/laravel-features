3ffdbff6bbd15ae90923cd352b6e57db6c6baed1
Danish Mehmood <danishmehmood981@gmail.com>
2024-03-21 11:01:22 +0500

    adding more dummy code

---
+++ app/Http/Controllers/PortoController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PortoController extends Controller {
+    public function index() {
+
+    }
+
+    public function create() {
+    }
+
+    public function about() {
+    }
+
+    public function show($id) {
+
+    }
+
+    public function edit($id) {
+
+    }
+
+    public function update(Request $request) {
+    }
+
+    public function destroy($id) {
+
+    }
+
+    public function portfolio($id) {
+
+    }
+
+    public function portfolioDetails() {
+
+    }
+
+}
